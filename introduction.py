d60d4a1e963259705aa903ceb1bee0035604418a
morrinegitari <morrinegitari@gmail.com>
2025-02-03 16:23:15 +0300

    py commit

---
+++ introduction.py
@@ -0,0 +1,6 @@
+#print is a method used to display an output
+
+print("Morrine")
+print("eMobilis")
+print(100)
+print(10+20)
---
+++ variable.py
@@ -0,0 +1,23 @@
+#variable is a container used to store a value
+
+
+location = "westlands"
+firstname = "Morrine"
+lastname = "Gitari"
+school = "eMobilis"
+x = 39
+y = 2
+print(location)
+print(firstname)
+print(lastname)
+print(school)
+
+print(firstname,lastname)
+print("My fullname is",firstname,lastname)
+print(school,"is a coding school")
+
+print(x+y)
+#the product of 39 and 2 is 78
+m=39
+x=2
+print("the product of x and m is",m*x)
