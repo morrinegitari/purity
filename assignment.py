fcc62306cbd242dce2f9d9f2832c6b6edcc0ef74
morrinegitari <morrinegitari@gmail.com>
2025-02-06 14:15:50 +0300

    normal commit

--- assignment.py
+++ assignment.py
@@ -1,13 +1,14 @@
 # A simple python program to check whether a year is a leap year or not
 
-year = 2025
-if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-    print(year "is a leap year ")
-else:
-    print(year "is not a leap year")
+year = int(input("Enter the year"))
+if year % 400 == 0:
+    print(year,"is a leap year ")
+elif  year % 4 == 0 and year % 100 != 0 :
+    print(year ,"is  a leap year")
+else :
+  print(year ,"is not leap year")
 
-
-#python program to check whethera letter is vowel or consonant
+#python program to check whether a letter is vowel or consonant
 
 letter ="A"
 if  (letter == 'a' or letter == 'e'or letter =="i" or letter == "o" or letter =="u" or letter == "A" or letter == "E" or letter == "I" or letter =="O" or letter =="U" ):
